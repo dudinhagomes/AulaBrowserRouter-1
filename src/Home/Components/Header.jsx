fe2d7dd2682a8d06ee02c16eb75071ab1744dd1a
dudinhagomes <maria.cruz6@estudante.ifms.edu.br>
2024-08-09 16:16:01 -0400

    finalizando a aula

---
+++ src/Home/Components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+    return(
+        <header>
+            <link to="./home">
+              <li>Home</li>
+            </link>
+            <link to="./Produto">
+              <li>Produto</li>
+            </link>
+            <link to="./ofertas">
+              <li>ofertas</li>  
+            </link>
+        </header>
+
+    );
+}
