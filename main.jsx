7a7623ef5c42779c75f03e8b7c88f856090d7332
dudinhagomes <161074365+dudinhagomes@users.noreply.github.com>
2024-08-22 16:34:24 -0400

    tentativa 22/08

--- main.jsx
+++ main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import  ReactDOM from 'react-dom/client'
 import { BrowserRouter , Routes} from 'react-router-dom'
 import Home from "./home"
+import Produto from "./Produto"
+import Oferta from "./Oferta"
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Routes>
             <Route path="/home" element={<Home/>}/>
             <Route path="/Produto" element={<Produto/>}/>
-            <Route path="/Ofeerta" element={<Oferta/>}/>
+            <Route path="/Oferta" element={<Oferta/>}/>
           </Routes>
         </BrowserRouter>
     </React.StrictMode>
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Header from '../Componets/header'
 export default function Home (){
     return(
      <>
@@ -11,12 +12,13 @@ export default function Home (){
         showStatus={false}
         showThumbs={false}
         >
-        <div src ="https://lojavivara.vtexassets.com/assets/vtex.file-manager-graphql/images/2cfc4a3d-d275-4611-8af8-15ea26ea3294___59bdaf2ba16c90457385957422dba34b.png">
+        <div>
+         <img src ="https://lojavivara.vtexassets.com/assets/vtex.file-manager-graphql/images/2cfc4a3d-d275-4611-8af8-15ea26ea3294___59bdaf2ba16c90457385957422dba34b.png"/>
         </div>
-        <div src ="https://lojavivara.vtexassets.com/assets/vtex.file-manager-graphql/images/2cfc4a3d-d275-4611-8af8-15ea26ea3294___59bdaf2ba16c90457385957422dba34b.png">
+        <div>
+        <img src ="https://lojavivara.vtexassets.com/assets/vtex.file-manager-graphql/images/2cfc4a3d-d275-4611-8af8-15ea26ea3294___59bdaf2ba16c90457385957422dba34b.png"/>
             </div>
-        
     </Carousel>
      </>
-    )
+    );
 }
